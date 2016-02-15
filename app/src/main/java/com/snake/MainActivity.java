1082c1923252d60261b116796531a19e34a15e45
Denis Nikolaev <idenisnikolaev@gmail.com>
2016-02-15 17:54:14 +0300

    modified activity_lose

--- app/src/main/java/com/snake/MainActivity.java
+++ app/src/main/java/com/snake/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends ActionBarActivity {
 
+    // 0 - первый запуск
+    // 1 - запуск после проигрыша
+    public static int GAME_MODE = 0;
+    public static int GAME_SCORE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
